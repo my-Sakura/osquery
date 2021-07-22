a799f6069aeb8fed11118d099430b65bd05fb9f2
shuaiqidechuan <1335917149@qq.com>
2021-07-22 19:24:44 +0800

    11

--- req/req.go
+++ req/req.go
@@ -9,6 +9,24 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Table struct {
+	Datetime      time.Time `json:"datetime"`
+	Day           string    `json:"day"`
+	Hour          string    `json:"hour"`
+	Iso8601       time.Time `json:"iso_8601"`
+	LocalTime     string    `json:"local_time"`
+	LocalTimezone string    `json:"local_timezone"`
+	Minutes       string    `json:"minutes"`
+	Month         string    `json:"month"`
+	Seconds       string    `json:"seconds"`
+	Timestamp     string    `json:"timestamp"`
+	Timezone      string    `json:"timezone"`
+	UnixTime      string    `json:"unix_time"`
+	Weekday       string    `json:"weekday"`
+	WinTimestamp  string    `json:"win_timestamp"`
+	Year          string    `json:"year"`
+}
+
 type Result struct {
 }
 
@@ -19,13 +37,13 @@ func (a *Result) Method(r gin.IRouter) {
 	r.GET("/", a.OsQueryi)
 }
 func (a *Result) OsQueryi(c *gin.Context) {
-	var result []byte
-	var err error
-	cmd := exec.Command("osqueryi", "--json", "SELECT * FROM time")
-	if result, err = cmd.Output(); err != nil {
-		fmt.Println(err)
+	cmd := exec.Command("osqueryi", "--json",
+		"SELECT weekday, year, month, day, hour, minutes, seconds, timezone, local_time, timestamp, datetime, isl_8601 FROM time")
+	result, err := cmd.Output()
+	if err != nil {
+		log.Println(err)
 	}
-	var data interface{}
+	var data Table
 	json.Unmarshal(result, &data)
 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": data})
 }
