c22314a6d083585f666e41c80a09d705a07deedc
shuaiqidechuan <1335917149@qq.com>
2021-07-23 13:43:56 +0800

    11

--- osquery/controller/controller.go
+++ osquery/controller/controller.go
@@ -19,27 +19,27 @@ func (oc *OsqueryController) Register(r gin.IRouter) {
 	r.GET("/time", oc.time)
 }
 
-// func (oc *OsqueryController) mounts(c *gin.Context) {
-// 	cmd := exec.Command("osqueryi", "--json", "SELECT * FROM mounts;")
-// 	result, err := cmd.Output()
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
-// 		return
-// 	}
+func (oc *OsqueryController) mounts(c *gin.Context) {
+	cmd := exec.Command("osqueryi", "--json", "SELECT * FROM mounts;")
+	result, err := cmd.Output()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
+		return
+	}
 
-// 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": string(result)})
-// }
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": string(result)})
+}
 
-// func (oc *OsqueryController) systemInfo(c *gin.Context) {
-// 	cmd := exec.Command("osqueryi", "--json", "SELECT * FROM system_info")
-// 	result, err := cmd.Output()
-// 	if err != nil {
-// 		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
-// 		return
-// 	}
+func (oc *OsqueryController) systemInfo(c *gin.Context) {
+	cmd := exec.Command("osqueryi", "--json", "SELECT * FROM system_info")
+	result, err := cmd.Output()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"status": http.StatusInternalServerError})
+		return
+	}
 
-// 	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": string(result)})
-// }
+	c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": string(result)})
+}
 func (oc *OsqueryController) time(c *gin.Context) {
 	cmd := exec.Command("osqueryi", "--json",
 		"SELECT weekday, year, month, day, hour, minutes, seconds, timezone, local_time, timestamp, datetime, isl_8601 FROM time")
